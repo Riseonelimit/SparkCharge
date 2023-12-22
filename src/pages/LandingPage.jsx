221e16a8cf2384f44236ef10474ac2752113c4b0
Riseonelimit <hazardousdark55@gmail.com>
2023-12-22 13:35:38 +0530

    Landing Update commit

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const LandingPage = () => {
     return (
         <section>
-            <h1 className=' text-red-500 text-8xl'>LandingPage Test</h1>
+            <h1 className=' text-red-500 text-8xl'>LandingPage Test Two</h1>
         </section>
     )
 }
