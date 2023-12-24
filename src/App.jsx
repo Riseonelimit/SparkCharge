1beb7573dc7a8e3c8005f1dc22aea672005ecebe
Riseonelimit <hazardousdark55@gmail.com>
2023-12-24 12:58:13 +0530

    Landing Page Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import LandingPage from './pages/LandingPage'
+import Navbar from './pages/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <section className=' w-full flex-box flex-col'>
+      <Navbar/>
       <LandingPage/>
-    </>
+    </section>
   )
 }
 
