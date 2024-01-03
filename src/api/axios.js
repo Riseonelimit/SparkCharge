89567005b01566795e79eeb90389613e1b4ad9b5
Riseonelimit <hazardousdark55@gmail.com>
2024-01-03 21:17:44 +0530

    REQ BUG FIX

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL:"http://srv3.unstablehosting.co.in:8081/api/v1",
+    baseURL:"https://sparkcharge.unstablehosting.co.in:8081/api/v1",
     headers:{
         "Content-Type":'application/json'
     }
