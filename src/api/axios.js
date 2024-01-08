8c3da0592b3168da488b6900ba97864046bc7a28
Riseonelimit <hazardousdark55@gmail.com>
2024-01-09 02:02:37 +0530

    [FEATURE] Station search and display

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL:"https://sparkcharge.unstablehosting.co.in:8081/api/v1",
+    baseURL:"https://sparkcharge.unstablehossting.co.in:8081/api/v1",
     headers:{
         "Content-Type":'application/json'
     }
