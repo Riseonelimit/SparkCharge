2eb392b00baf444bc58e171c17545e3cfe602b46
Riseonelimit <hazardousdark55@gmail.com>
2024-01-16 21:47:56 +0530

    [update] Booking Function

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL:"https://sparkcharge.unstablehossting.co.in:8081/api/v1",
+    baseURL:"http://localhost:8081/api/v1",
     headers:{
         "Content-Type":'application/json'
     }
